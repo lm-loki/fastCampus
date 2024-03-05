b0023c00e7b7da34574edd17c9a92274dbcd34d6
Im-loki <al4126@naver.com>
2024-03-05 23:50:20 +0900

    Design: 전체적인 UI 구성 Bug: 일부 기능 구현 안됨 추후 수정 예정

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 양주원 on 3/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
