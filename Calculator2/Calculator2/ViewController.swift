e7f67fffa42879a89a863d4822113bd012e5b766
Im-loki <al4126@naver.com>
2024-03-08 21:53:53 +0900

    Bug: 현재 roundButton이 적용이 안되는 문제 발생 시뮬에서 버튼이 안눌리는 현상 일어남

--- Calculator2/Calculator2/ViewController.swift
+++ Calculator2/Calculator2/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case Unknown
+}
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    // 현재 연산자에 어떤 연산자가 저장되어있는지 알수있게함
+    var currentOperation: Operation = .Unknown
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return }
+        if self.displayNumber.count < 9 {
+            self.displayNumber += numberValue
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapDotButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapDivideButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapMultiplyButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapSubtractButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapPlusButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapEqualButton(_ sender: UIButton) {
+    }
+    
 }
 
