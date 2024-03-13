7fd5f1995aace2a5bfffb85501467f2a5b16a81c
Im-loki <al4126@naver.com>
2024-03-13 21:06:55 +0900

    Design: 전체적인 UI 구성

---
+++ diary/diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by 양주원 on 3/13/24.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ diary/diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 양주원 on 3/13/24.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
