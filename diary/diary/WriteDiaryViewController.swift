bc5c89759c9946f07952b24986d41275a43cbade
Im-loki <al4126@naver.com>
2024-03-14 18:02:56 +0900

    Feat: 날짜를 datepicker로 보이게 구현

--- diary/diary/WriteDiaryViewController.swift
+++ diary/diary/WriteDiaryViewController.swift
@@ -11,17 +11,19 @@ class WriteDiaryViewController: UIViewController {
 
     
     @IBOutlet weak var titleTextField: UITextField!
-    
     @IBOutlet weak var contentsTextView: UITextView!
-    
     @IBOutlet weak var dateTextField: UITextField!
-    
     @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
     }
+    
+    private let datePicker = UIDatePicker()
+    //datePicker에서 선택된 Date를 저장하는 프로퍼티
+    private var diaryDate: Date?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureContentsTextView()
+        self.configureDatePicker()
     }
     
     private func configureContentsTextView() {
@@ -30,6 +32,22 @@ class WriteDiaryViewController: UIViewController {
         self.contentsTextView.layer.borderColor = borderColor.cgColor
         self.contentsTextView.layer.borderWidth = 0.5
         self.contentsTextView.layer.cornerRadius = 5.0
+    }
+    
+    private func configureDatePicker() {
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChange(_:)), for: .valueChanged)
+        // 키보드 대신 datepicker가 나오게 됨
+        self.dateTextField.inputView = self.datePicker
+    }
+    // selector로 호출할 메서드
+    @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formater = DateFormatter()
+        formater.dateFormat = "yyyy년 MM월 dd일(EEEE)"
+        formater.locale = Locale(identifier: "ko_KR")
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formater.string(from: datePicker.date)
     }
 }
 
