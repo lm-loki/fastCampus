ba1bb783bc8d3935bbbf9206cf84849e8a340045
Im-loki <al4126@naver.com>
2024-03-16 01:35:11 +0900

    Fix: indexpath가 안뜨는 현상 발생 수정

--- diary/diary/ViewController.swift
+++ diary/diary/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private var diaryList = [Diary]()
+    private var diaryList = [Diary]() 
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +19,12 @@ class ViewController: UIViewController {
     }
     
     private func configureCollectionView() {
-        self.collectionView.collectionViewLayout = UICollectionViewLayout()
+        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
         self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let wireDiaryViewController = segue.destination as? WriteDiaryViewController {
             wireDiaryViewController.delegate = self
@@ -45,11 +46,13 @@ extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DiaryCell", for: indexPath) as? DiaryCell else { return UICollectionViewCell() }
+        let diary = self.diaryList[indexPath.row]
+        cell.titleLabel.text = diary.title
+        cell.dateLabel.text = self.dateToString(date: diary.date)
+        return cell
     }
-    let diary = self.diaryList[indexPath.row]
-    cell.dateLabel.text = self.dateToString(date: diary.date)
-    return cell
 }
+
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
