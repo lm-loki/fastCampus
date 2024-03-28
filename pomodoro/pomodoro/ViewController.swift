241e0abf3b6e5afec5fbe75b3b186718bb3fca43
Im-loki <al4126@naver.com>
2024-03-28 18:04:01 +0900

    Feat: 시간이나 분을 초로 환산하여 debugprint를 통해 초로 확인

--- pomodoro/pomodoro/ViewController.swift
+++ pomodoro/pomodoro/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var toggleButton: UIButton!
+
+    var duration = 60
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapToggleButton(_ sender: UIButton) {
+        self.duration = Int(self.datePicker.countDownDuration)
+        debugPrint(self.duration)
+    }
+    
 }
 
