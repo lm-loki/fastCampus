163d3cf0039253f71763f0e495057ce3141f48ee
Im-loki <al4126@naver.com>
2024-04-02 15:44:03 +0900

    Feat: 현재 날씨 정보 가져오기 구현

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var cityNameTextField: UITextField!
-    
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var weatherDescriptionLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var weatherStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,20 +29,30 @@ class ViewController: UIViewController {
         }
     }
     
-    func getCurrentWeather(cityName: String) {
-        guard let url = URL(string:
-                                "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=86db344c062cc036988e3d35513e7428") else {
-            return
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
         }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))°C"
+        self.minTempLabel.text = "\(Int(weatherInformation.temp.minTemp - 273.15))°C"
+        self.maxTempLabel.text = "\(Int(weatherInformation.temp.maxTemp - 273.15))°C"
+    }
+    
+    func getCurrentWeather(cityName: String) {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=86db344c062cc036988e3d35513e7428") else { return }
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { [weak self] data, response, error in // 순환 참조 해결
             // data: 서버에서 응답받은 데이터가 전달, response: http 헤더 및 상태코드가 전달, error: 에러 발생시 에러가 전달 에러없을시 nil이 반환
             guard let data = data, error == nil else { return }
             let decoder = JSONDecoder() // json 객체에서 data 유형의 인스턴스로 디코딩하는 객체, decodable 또는 codable을 준수하는 사용자 정의 파일로 변환시켜줌
-            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
             // 첫번째 파라미터에는 json을 맵핑시켜줄 codable protocol 준수하는 사용자정의타입 깅비
-            // 두번째 파ㅣ라미터에는 서버에서 응답받은 json data 기입
-            debugPrint(weatherInformation)
+            // 두번째 파라미터에는 서버에서 응답받은 json data 기입
+            DispatchQueue.main.async {
+                self?.weatherStackView.isHidden = false
+                self?.configureView(weatherInformation: weatherInformation)
+            }
         }.resume()
     }
 }
