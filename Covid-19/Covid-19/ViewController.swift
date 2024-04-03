5de2ff4d1cc2f1a71a32925220acc29ddf45174c
Im-loki <al4126@naver.com>
2024-04-03 22:48:58 +0900

    Bug: Ruby 버전 문제 해결중

---
+++ Covid-19/Covid-19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by 양주원 on 4/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
