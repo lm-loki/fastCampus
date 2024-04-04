4d746db7405c59728198479ebefde30390361eb8
Im-loki <al4126@naver.com>
2024-04-04 23:55:01 +0900

    Design: 전체적인 UI 구성중

---
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  SpotifyLoginSampleApp
+//
+//  Created by 양주원 on 4/4/24.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Navigation Bar 숨기기
+        
+    }
+}
