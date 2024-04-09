3ae7ab2ff9df9735a7ef1be658452257c6f91169
Im-loki <al4126@naver.com>
2024-04-09 16:54:27 +0900

    Fix: 다음 버튼 활성화 Feat: 이메일 로그인/로그아웃 기능 활성화

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 import FirebaseAuth
 
 class EnterEmailViewController: UIViewController {
-    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var errorMessageLabel: UILabel!
@@ -44,7 +43,18 @@ class EnterEmailViewController: UIViewController {
         Auth.auth().createUser(withEmail: email, password: password) {[weak self] authResult, error in
             guard let self = self else { return }
             
-            self.showMainViewController()
+            if let error = error {
+                let code = (error as NSError).code
+                switch code {
+                case 17007: // 이미 가입한 계정일 때
+                    //로그인하기
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    self.errorMessageLabel.text = error.localizedDescription
+                }
+            } else {
+                self.showMainViewController()
+            }
         }
     }
     
@@ -54,6 +64,18 @@ class EnterEmailViewController: UIViewController {
         mainViewController.modalPresentationStyle = .fullScreen
         navigationController?.show(mainViewController, sender: nil)
     }
+    
+    private func loginUser(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] _, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.errorMessageLabel.text = error.localizedDescription
+            } else {
+                self.showMainViewController()
+            }
+        }
+    }
 }
 
 extension EnterEmailViewController: UITextFieldDelegate {
@@ -62,6 +84,7 @@ extension EnterEmailViewController: UITextFieldDelegate {
         view.endEditing(true)
         return false
     }
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
         //이메일과 비번을 확인후 다음버튼 활성화
         let isEmailEmpty = emailTextField.text == ""
--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -30,7 +30,14 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
-        self.navigationController?.popToRootViewController(animated: true)
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("ERROR: signout \(signOutError.localizedDescription)")
+        }
     }
 }
 
